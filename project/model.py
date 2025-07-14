de3c315edb3314c291e4c99f552c497fa2206ee7
nonseww <innapolituchaya@gmail.com>
2025-07-15 02:22:22 +0300

    feat(model): build the skeleton of the model program

---
+++ project/model.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import keras
+
+
+# Creates and compiles a linear regression model
+def build_model(learning_rate, num_features):
+    pass
+
+
+# Trains the given model
+def train_model(model, features, label, epochs, batch_size):
+    pass
+
+
+# Generates a formatted string summary about th trained linear model
+def model_info(feature_names, label_name, model_output):
+    pass
+
+
+# Core of the model: all functions are called from here
+def run_experiment(df, feature_names, label_name, learning_rate, epochs, batch_size):
+    pass
