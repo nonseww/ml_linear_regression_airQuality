8821841682b57f4994962b4bbf962c754e8d0dad
nonseww <innapolituchaya@gmail.com>
2025-07-15 23:49:15 +0300

    feat(prediction): write the function runs trained model on a random batch of data

--- project/prediction.py
+++ project/prediction.py
@@ -16,7 +16,22 @@ def build_batch(df, batch_size):
 
 # Runs trained model on a random batch of data
 def predict_fare(model, df, features, label, batch_size=50):
-    pass
+    batch = build_batch(df, batch_size)
+    predicted_values = model.predict_on_batch(x=batch.loc[:, features].values)
+    data = {"PREDICTED_CO": [], "OBSERVED_CO": [], "L1_LOSS": [], features[0]: [], features[1]: [], features[2]: []}
+
+    for i in range(batch_size):
+        predicted = predicted_values[i][0]
+        observed = batch.at[i, label]
+        data["PREDICTED_CO"].append(format_currency(predicted))
+        data["OBSERVED_CO"].append(format_currency(observed))
+        data["L1_LOSS"].append(format_currency(abs(observed - predicted)))
+        data[features[0]].append(format_currency(batch.at[i, features[0]]))
+        data[features[1]].append(format_currency(batch.at[i, features[1]]))
+        data[features[2]].append(format_currency(batch.at[i, features[2]]))
+
+    output_df = pd.DataFrame(data)
+    return output_df
 
 
 # Prints info about prediction
